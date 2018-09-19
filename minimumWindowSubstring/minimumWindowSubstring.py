e806dbe75bbe197dd701c380050130e70dfd1e27
jmloewen <jonoloewen@gmail.com>
2018-09-19 16:01:21 -0700

    uploading a suboptimal minwindowsubstr

---
+++ minimumWindowSubstring/minimumWindowSubstring.py
@@ -0,0 +1,62 @@
+#An unoptimized minimumWindowSubstring that is somehow a 95% solution.
+class Solution:
+    def minWindow(self, s, t):
+        from collections import defaultdict
+        #Time for sliding window
+        #A simple sliding window - monitor left and right, scan each intermediary value to find whatever middle value we need.
+        #left and right edges of windows will include left and right edges of T.  need to find intermediary values within substring of S, in no particular order.
+        #Find t within s.
+        #Find start of t.
+        sizeT = len(t)
+        keysToFill = len(set(t))
+        keysFilled = 0
+        charDict = defaultdict(lambda: 0)
+        winDict = defaultdict(lambda: 0)
+        ans = None
+
+        #Fill our charDict with characters that are in t.
+        for c in t:
+            charDict[c] += 1
+
+        left = right = 0
+
+        #Move right edge to the right.  Once conditions are fulfilled by moving to the right, move left rightwards until the next movement would make the condition no longer fulfilled.
+        while right < len(s):
+            cur = s[right]
+            if charDict[cur] > 0:#If this character is in the charDict, stack it up
+                winDict[cur] += 1
+                #If the amount of this character in cD == it in wD, check off a requirement as filled.
+                if winDict[cur] == charDict[cur]:
+                    keysFilled += 1
+            else:
+                #Then it's not in the charDict.  continue rightwards.
+                pass
+
+            if keysFilled == keysToFill:
+                #We've found an answer.  Move left to the right until it's in the smallest possible still valid state.
+                while left < right:
+                    curLeft = s[left]
+                    #If removing this value would not reduce us to an insolvent state, do it.
+                    if winDict[curLeft] > charDict[curLeft] and charDict[curLeft] > 0:
+                        winDict[curLeft] -= 1
+                    elif winDict[curLeft] == charDict[curLeft] and charDict[curLeft] >= 1:
+                        break
+                    left += 1
+
+                if not ans or right - left < len(ans):
+                    ans = s[left:right + 1]
+
+
+                if len(ans) == len(t):
+                    return ans
+
+                #Adjust left rightwards to continue the check.\
+                winDict[s[left]] -= 1
+                keysFilled -= 1
+
+                left += 1
+            right += 1
+
+        if ans:
+            return ans
+        return ""
