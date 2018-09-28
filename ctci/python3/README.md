4018ae5dbebe579c25bc8495bcb22e0218f045d7
jmloewen <jonoloewen@gmail.com>
2018-09-27 17:17:07 -0700

    starting CTCI, in python to begin.  Compiled in Leetcode playground.

---
+++ ctci/python3/README.md
@@ -0,0 +1,4 @@
+Running through CTCI problems in preparation for an upcoming Google interview.
+All code here was first run and verified through the leetbox playground at https://leetcode.com/playground/new/empty
+
+This folder is for Python3 implementations.
---
+++ ctci/python3/arrayAndStrings/arrayAndStrings.py
@@ -0,0 +1,61 @@
+#1.1: Implement an algorithm to determine if a string has all unique characters.
+    #1.1.2: What if we cannot use additional data structures?
+
+    #Thoughts?
+    #Make a dict.  Store everything in individual Dict entries.  If value is in dict, return F.
+    #O(s) time, O(s) storage.
+
+    #String is not sorted.
+    #If it were sorted on ASCII value, could simply check to see if adjacent values were equal.  Would be O(s) time, O(1) space.
+
+    #For unsorted, dict approach seems to be the right one, even though it's the way that I've always done this problem.
+class Solution():
+    def isUnique(self, S):
+        ltrDict = {}
+
+        for ltr in S:
+            if ltrDict.get(ltr):
+                return False
+            ltrDict[ltr] = 1
+        return True
+
+    #No additional data structures - does this mean no Arrays as well?  Only self comparison?
+    #Could sort and search - NlogN
+    def isUniqueNoExtra(self, S):
+        sortS = ''.join(sorted(S))
+        prev = "ab"
+        for ltr in sortS:
+            if ltr == prev:
+                return False
+            prev = ltr
+        return True
+
+def testSolution(F, expected, testNumber):
+    if F != expected:
+        print("Test Number: ", testNumber, " Failed.")
+        return False
+    return True
+
+#Examples:
+        #S="12345678901" F
+        #S="11234567890" F
+        #S="1234567890a" T
+        #S="" T
+        #S="a" T
+        #S="aA" Probably T.  Depends on what the question wants.
+
+s = Solution()
+
+testSolution(s.isUnique("12345678901"), False, 0)
+testSolution(s.isUnique("11234567890"), False, 1)
+testSolution(s.isUnique("1234567890a"), True, 2)
+testSolution(s.isUnique(""), True, 3)
+testSolution(s.isUnique("a"), True, 4)
+testSolution(s.isUnique("aA"), True, 5)
+
+testSolution(s.isUniqueNoExtra("12345678901"), False, 6)
+testSolution(s.isUniqueNoExtra("11234567890"), False, 7)
+testSolution(s.isUniqueNoExtra("1234567890a"), True, 8)
+testSolution(s.isUniqueNoExtra(""), True, 9)
+testSolution(s.isUniqueNoExtra("a"), True, 10)
+testSolution(s.isUniqueNoExtra("aA"), True, 11)
